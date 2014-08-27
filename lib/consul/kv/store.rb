da855b3d67ffda7f60a6ab0fd76ace6b345259b8
JGW Maxwell <jgwmaxwell@gmail.com>
2014-08-27 18:53:28 +0100

    allow scoping a store

--- lib/consul/kv/store.rb
+++ lib/consul/kv/store.rb
@@ -8,22 +8,23 @@ module Consul
       CannotWriteError  = Class.new(StandardError)
       CannotDeleteError = Class.new(StandardError)
 
-      def initialize
-        @mutex = Mutex.new
-        @conn  = Connection.new
+      def initialize(prefix = nil)
+        @mutex  = Mutex.new
+        @conn   = Connection.new
+        @prefix = prefix
       end
 
       def get(key)
         resp = nil
         @mutex.synchronize do
-          resp = conn.get key, true
+          resp = conn.get build(key), true
         end
         return nil if resp.kind_of?(Net::HTTPNotFound)
         raise CannotReadError unless resp.kind_of?(Net::HTTPSuccess)
         json = JSON.parse(resp.body)
-        if json.size > 1 || json.first['Key'] != key
+        if json.size > 1 || json.first['Key'] != build(key)
           json.each_with_object({}) do |item, hsh|
-            hsh[item['Key'].gsub(/\A#{key}\//, '')] = Base64.decode64(item['Value'])
+            hsh[item['Key'].gsub(/\A#{build(key)}\//, '')] = Base64.decode64(item['Value'])
           end
         else
           Base64.decode64 json.first['Value']
@@ -34,7 +35,7 @@ module Consul
       def put(key, value)
         resp = nil
         @mutex.synchronize do
-          resp = conn.put key, value
+          resp = conn.put build(key), value
         end
         raise CannotWriteError unless resp.kind_of?(Net::HTTPSuccess)
         get key
@@ -44,14 +45,21 @@ module Consul
       def delete(key)
         resp = nil
         @mutex.synchronize do
-          resp = conn.delete key, true
+          resp = conn.delete build(key), true
         end
         raise CannotDeleteError unless resp.kind_of?(Net::HTTPSuccess)
+        true
       end
 
       private
 
       attr_reader :conn
+
+      def build key
+        return key unless @prefix
+        return @prefix if key == ''
+        "#{@prefix}/#{key}"
+      end
 
     end
   end
